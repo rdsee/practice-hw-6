981f742385aa5fa82fd24f4bf889799d601f65c5
rdsee <rost.slobod@gmail.com>
2026-04-06 14:59:06 +0300

    copy logics testing

--- practice.py
+++ practice.py
@@ -7,3 +7,19 @@
 # print(tuple1)
 #
 
+# import copy
+#
+# info = 123, 'll', 3.24
+# test = copy.deepcopy(info)
+# print(test)
+#
+# info_copy = info
+# print(info_copy)
+# info_list = list(info_copy)
+# print(info_list)
+# info_list.append('23')
+# print(info_list)
+# info = tuple(info_list)
+# print(info)
+# print(info[2:3])
+# print(info[0:3:2])
